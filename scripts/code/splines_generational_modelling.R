4003b07ab1fc806d8a9d5a67ded78b0e90bf5220
emilyblackk <84051085+emilyblackk@users.noreply.github.com>
2022-09-21 13:27:54 -0700

    Code Updates, and trying to add local maxima

--- scripts/code/splines_generational_modelling.R
+++ scripts/code/splines_generational_modelling.R
@@ -11,8 +11,8 @@
 rm(list=ls())
 
 #load relevant libraries for script
-pkgs <- c("tidyverse", "scales", "stats", "cowplot")
-install.packages(pkgs)
+pkgs <- c("tidyverse", "scales", "stats", "cowplot", "ggpmisc")
+#install.packages(pkgs)
 lapply(pkgs, library, character.only = TRUE)
 rm(pkgs)
 
@@ -62,49 +62,113 @@ obs <- obs %>%
 #Part 2. Determining patterns in abundance across dates, colours, and latitudes
 abundances <- obs %>%
   group_by(date, latitude_band, colour_morph) %>%
-  summarize(worm_count = n())          
+  summarize(worm_count = n())   
+
+#We have dates where no worms were observed, however our sum function is not counting them
+#Create tables with all possible dates, colour, and latitude band
+dates_table <- function(x, y) {
+ date_seq <- data.frame(seq(ymd("2018-01-01"), ymd("2020-12-31"), by="day"))
+ colnames(date_seq) <- c("date")
+ colour <- x
+ lat_band <- y
+ date_table <- cbind(date = date_seq, colour_morph = colour, latitude_band =lat_band)
+}
+
+#set up tables for black webworm
+table_black_29 <- dates_table("Black", "29-33")
+table_black_33 <- dates_table("Black", "33-37")
+table_black_37 <- dates_table("Black", "37-41")
+table_black_41 <- dates_table("Black", "41-45")
+#Set up tables for red webworms
+table_red_29 <- dates_table("Red", "29-33")
+table_red_33 <- dates_table("Red", "33-37")
+table_red_37 <- dates_table("Red", "37-41")
+table_red_41 <- dates_table("Red", "41-45")
+
+#Bind all these columns together 
+date_list_table <- rbind(table_black_29,table_black_33, 
+                         table_black_37, table_black_41, 
+                         table_red_29,table_red_33, 
+                         table_red_37, table_red_41)
+#Clean the workspace
+rm(table_black_29,table_black_33, 
+   table_black_37, table_black_41, 
+   table_red_29,table_red_33, 
+   table_red_37, table_red_41)
+
+#Join the abundance table with the date list table to get full set of observations
+abundances_complete <- left_join(date_list_table, abundances) 
+#Replace NA with 0
+  abundances_complete[is.na(abundances_complete)] <- 0
+
+
 
 
 #Get relative abundance of worms on each date 
 #Bring back year column
-abundances$year <-format(as.Date(abundances$date, format="%Y-%m-%d"), "%Y")
-#Add week column
-abundances$week <- format(abundances$date, format="%W/%Y")
-
-#generate summary table of webworm abundances per latitude band, year, and colour
-summary_table_weeks <- abundances %>%
-  group_by(colour_morph, latitude_band, week) %>%
-  summarize(week_count = n())
-summary_table_weeks$year <-format(as.Date(summary_table_weeks$week, format="%W/%Y"), "%Y")
+abundances_complete$year <-format(as.Date(abundances_complete$date, format="%Y-%m-%d"), "%Y")
+#Add week  and month column
+abundances_complete$week <- lubridate::week(abundances_complete$date)
+abundances_complete$month <- lubridate::month(abundances_complete$date)
 
 
-#generate summary table of webworm abundances per latitude band, year, and colour
-summary_table_year <- abundances %>%
-  group_by(colour_morph, latitude_band, year) %>%
-  summarize(total_count = n())
+#generate summary table of webworm abundances_complete per latitude band, year, and colour
+summary_table_weeks <- abundances_complete %>%
+  group_by(colour_morph, latitude_band, year, month, week) %>%
+  summarize(week_count = sum(worm_count))
+
 
 #Use left-join to add total counts to each observation
-abundances_2 <- 
-  left_join(summary_table_weeks, summary_table_year,
-            by=c("year", "latitude_band", "colour_morph"))
-
-#Get relative abundance on each date
-abundances_2 <- abundances_2 %>%
-  mutate(rel_count=week_count/total_count)
+#get relative counts
+#scale red and black data so peaks can appear
+abundances_2 <- summary_table_weeks %>%
+group_by(colour_morph, latitude_band, year) %>%
+  mutate(scaled_count = 
+           rescale(week_count, to = c(0, 1), from = range(week_count)))
 #Add date column for plotting
+abundances_2$date <- as.Date(make_datetime(year=abundances_2$year)+
+weeks((abundances_2$week-1)))
+
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+
+library(stats)
+#Part 3. Identify local maxima for ggplot
+abundances_2 <- abundances_2 %>% 
+  group_by(colour_morph, latitude_band, year) %>%
+  mutate(smooth_y = predict(loess(scaled_count~as.numeric(date), span=0.1), 
+                            newdata=as.numeric(date)))
+identify_maxima <- function(x, y) {
+#identify maxima
+#From https://stackoverflow.com/questions/65442224/find-all-local-maxima-of-a-geom-smooth-curve-in-r-ggplot
+# Run length encode the sign of difference
+rle <- rle(diff(as.vector(x)) > 0)
+# Calculate startpoints of runs
+starts <- cumsum(rle$lengths) - rle$lengths + 1
+# Take the points where the rle is FALSE (so difference goes from positive to negative) 
+maxima_id <- starts[!rle$values]
+maximum <- y[maxima_id]
+}
+maxima_black_29 <- abundances_2 %>%
+  filter(colour_morph == "Black", latitude_band == "41-45")
+ maxima_black_29 <-  identify_maxima (maxima_black_29$smooth_y, 
+                                      as.numeric(maxima_black_29$date))
 
 
 #Part 3. Plot the Data
+
+#Factor the latitude bands so they are arranged North to South
+abundances_2$lat_factor = factor(abundances_2$latitude_band, 
+                                 levels=c('41-45','37-41','33-37','29-33'))
 plot <- abundances_2 %>%
-  filter(year==2020) %>%
-  ggplot(aes(x=week, y=rel_count))+
+  ggplot(aes(x=date, y=scaled_count))+
   geom_point(aes(colour=colour_morph),position="jitter", alpha=0.5)+
-  facet_wrap(vars(latitude_band), nrow=4) +
-  ylim(0, 0.3)+
+  facet_wrap(vars(lat_factor), nrow=4) +
+  ylim(0,1)+
   scale_color_manual(values = c("black", "#9C0260"))+
   labs(x="Date", y="Observations")+
   scale_linetype_manual(values=c("solid", "solid"))+
-  geom_smooth(aes(colour=colour_morph, linetype=colour_morph), method="loess", 
-              se=TRUE, fill="#34595E", span=0.25, lwd=2.5)+
+  geom_smooth(aes(colour=colour_morph, linetype=colour_morph 
+                  ), method="loess", span=0.1)+
   theme_classic()
 plot
